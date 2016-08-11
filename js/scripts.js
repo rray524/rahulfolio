222fa441d06b46c178c4c7f597c296633c40f54b
rahul <rray524@gmail.com>
2016-08-11 21:47:59 +0600

    rahulfolio
    it's a demo

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+(function () {
+    "use strict";
+
+    // custom scrollbar
+
+    $("html").niceScroll({
+        styler: "fb",
+        cursorcolor: "rgb(16, 167, 175)",
+        cursorwidth: '6',
+        cursorborderradius: '10px',
+        background: '#424f63',
+        spacebarenabled: false,
+        cursorborder: '0',
+        zindex: '1000'
+    });
+
+
+
+})(jQuery);
